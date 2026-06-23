dcc5862af4db5138314ab588fbdb3e983c0775a5
Рябухина Владислава <leduck7@rambler.ru>
2026-06-23 22:32:12 +0300

    Switch from EasyOCR to pytesseract for stable deployment

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,9 @@
-FROM rust:1.80 AS rust-builder
-
 FROM python:3.11-slim
 
-COPY --from=rust-builder /usr/local/cargo /usr/local/cargo
-ENV PATH="/usr/local/cargo/bin:${PATH}"
-
 RUN apt-get update && apt-get install -y \
+    tesseract-ocr \
     libgl1-mesa-glx \
     libglib2.0-0 \
-    libsm6 \
-    libxext6 \
-    libxrender-dev \
     && rm -rf /var/lib/apt/lists/*
 
 WORKDIR /app
--- app/core/ocr.py
+++ app/core/ocr.py
@@ -1,20 +1,27 @@
-import easyocr
-
+import numpy as np
+import cv2
+import pytesseract
 from app.core.utils import clean_and_merge_ingredients
 
-reader = easyocr.Reader(['en'], gpu=True)
 
+def ocr_image(image_bytes: bytes):
 
-def ocr_image(image):
-    results = reader.readtext(image, detail=1)
-    detected_texts = [text for (_, text, prob) in results if prob > 0.3]  # фильтр по уверенности
+    nparr = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
+
+    gray = cv2.cvtColor(nparr, cv2.COLOR_BGR2GRAY)
+
+    # gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+    custom_config = r'--oem 3 --psm 6'
+    text = pytesseract.image_to_string(gray, config=custom_config, lang='eng')
+
+    detected_texts = [line.strip() for line in text.split('\n') if line.strip()]
+
     ingredients = clean_and_merge_ingredients(detected_texts)
 
-    # для дебага оставим пока
     print(f"\nбыло считано строк: {len(detected_texts)} => стало чистых ингредиентов: {len(ingredients)}")
     print("\nчистый список ингредиентов:")
     for i, ing in enumerate(ingredients, 1):
         print(f"{i:2}. {ing}")
-    # удалить потом
 
     return ingredients
